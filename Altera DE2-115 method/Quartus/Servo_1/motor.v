6eafdddb187800f8480993207bdb4c2a568fb5de
Anjana Bandara <57900949+Anjanamb@users.noreply.github.com>
2023-04-26 15:31:23 +0530

    Modified the code for single server motor instance

--- Altera DE2-115 method/Quartus/Servo_1/motor.v
+++ Altera DE2-115 method/Quartus/Servo_1/motor.v
@@ -2,31 +2,34 @@ module motor(
     input mclk,
     input toggle,  //Use this as the control input
 	 output [0:0] Led,
-    output servo
+    output servo,
+	 output reg [15:0] data_out
     );
 
 //50 MHz clock onBoard
+//1 MHz input clock 
 //20 ms counter.
-//		1/50,000,000  Hz 			= 20  ns (every posedge)
-//		(20,000,000 ns)/(20 ns) = 1,000,000
+//		1/1,000,000  Hz 			= 1000  ns (every posedge)
+//		(20,000,000 ns)/(1,000 ns) = 20,000
 //
-//    (20 bits) (2^(20)-1) 	= 1,048,575 (from 0 to 1,048,575)
-//				Therefore, counter needs 20 bits [19:0]
-//				Count up to 999,999 (0 included)
+//    (15 bits) (2^(15)-1) 	= 32,767 (from 0 to 32,767)
+//				Therefore, counter needs 15 bits [14:0]
+//				Count up to 19,999 (0 included)
 
-//Assumed Max (180 deg) 2 ms 			= 100,000*20 ns (100,000 clks)
-//Assumed Min (0   deg) 1 ms 			=  50,000*20 ns ( 50,000 clks)
-//Positions  100,000 - 50,000 		= 50,000
-//Resolution (180 degrees)/50,000 	= 0.0036 degrees
+//Assumed Max (180 deg) 2 ms 			= 100,000*1000 ns (2,000 clks)
+//Assumed Min (0   deg) 1 ms 			=  50,000*1000 ns ( 1,000 clks)
+//Positions  2,000 - 1,000 		= 1,000
+//Resolution (180 degrees)/1,000 	= 0.18 degrees
 
 //essential registers
-reg [19:0] counter;
+reg [14:0] counter;
 reg        servo_reg;
+reg [7:0] data_reg	=	0; //255
 
 ////////////////////////////////////////////////////////////
 //Test Control registers ///////////////////////////////////
 ////////////////////////////////////////////////////////////
-reg [16:0] control	=	0; //131071 there is wiggle room but you should be careful to not overcontrol
+reg [11:0] control	=	0; //4095 there is wiggle room but you should be careful to not overcontrol
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
 
@@ -36,10 +39,10 @@ begin
 
 //Servo algorithm
 	counter <= counter + 1;
-	if(counter == 'd999999)
+	if(counter == 'd19999)
 			counter <= 0;
 
-	if(counter < ('d20000 + control))
+	if(counter < ('d400 + control))
 			servo_reg <= 1;
 	else
 			servo_reg <= 0;
@@ -53,15 +56,31 @@ begin
 	if(counter == 0)
 		begin
 			if(toggle == 0)
-					if(control == 0)
+					if(control == 0) 
+						begin
 							control <= control;
+							data_reg <= data_reg;
+							data_out <= {2'b01, 3'b000, data_reg[7:5], 2'b00, data_reg[4:0], 1'b0};
+						end
 					else
-							control <= control - 500;
+						begin
+							control <= control - 10;
+							data_reg <= data_reg - 1;
+							data_out <= {2'b01, 3'b000, data_reg[7:5], 2'b00, data_reg[4:0], 1'b0};
+						end
 			if(toggle == 1)
-					if(control == 'd110000)
+					if(control == 'd2200) 
+						begin
 							control <= control;
+							data_reg <= data_reg;
+							data_out <= {2'b01, 3'b000, data_reg[7:5], 2'b00, data_reg[4:0], 1'b1};
+						end
 					else
-							control <= control + 500;
+						begin
+							control <= control + 10;
+							data_reg <= data_reg + 1;
+							data_out <= {2'b01, 3'b000, data_reg[7:5], 2'b00, data_reg[4:0], 1'b1};
+						end
 		end
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
--- Altera DE2-115 method/Quartus/Servo_1/servo.v
+++ Altera DE2-115 method/Quartus/Servo_1/servo.v
@@ -1,15 +1,65 @@
 module servo(
     input mclk,
     input toggle,
-    output [0:0] Led1,
-    output pwm
+	 input wire Rdreq,
+	 input wire Wrreq,
+	 input wire Wr_en,
+    output [0:0] servo,
+    output pwm,
+	 output wire [15:0] Data_out,
+	 output wire Fifo_empty,
+	 output wire Fifo_full,
+	 output wire Tx,
+	 output wire Tx_busy
     );
+	 
+wire Txclk_en; 
+wire Clk_100hz;	 
+wire Clk_1mhz;
+wire Clk_200hz;
+wire [15:0] cir_in; //circuit data
+wire [7:0] Tx_data;
+assign Data_out = cir_in;
 
-motor motor(
-    .mclk(mclk),
-	 .toggle(toggle),
-    .Led(Led1),
-	 .servo(pwm)
-);
+clock_100hz clock_100hz ( .clk_50mhz(mclk),
+								  .clk_100hz(Clk_100hz)
+								  );	 
+								  
+clock_var clock_var ( .clk_50mhz(mclk),
+							 .clk_1mhz(Clk_1mhz)
+							 );	
+								  
+clk_200hz clk_200hz ( .clk_50mhz(mclk),
+							 .clk_200hz(Clk_200hz)
+							 );	 
+								  
+motor motor( .mclk(Clk_1mhz),
+				 .toggle(toggle),
+				 .Led(servo),
+				 .servo(pwm),
+				 .data_out(cir_in)
+				 );
 
+baudrate uart_baud(	.clk_50m(mclk),
+							.Txclk_en(Txclk_en)
+							);
+							
+transmitter uart_Tx(	.data_in(Tx_data),
+							.wr_en(Wr_en),
+							.clk_50m(mclk),
+							.clken(Txclk_en), //We assign Tx clock to enable clock 
+							.Tx(Tx),
+							.Tx_busy(Tx_busy)
+							);
+				 
+FIFO FIFO ( .rdclk(Clk_200hz),
+            .wrclk(Clk_100hz),
+				.data(cir_in),
+				.q(Tx_data),
+				.wrfull(Fifo_full),
+				.rdempty(Fifo_empty),
+				.rdreq(Rdreq),
+				.wrreq(Wrreq),				
+				);
+				 
 endmodule
