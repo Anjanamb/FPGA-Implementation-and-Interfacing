4ce623fb543e93727b19c10c09aa0a1694a10729
Anjana Bandara <57900949+Anjanamb@users.noreply.github.com>
2023-05-28 19:55:58 +0530

    Updated Data_rx.py

--- Altera DE2-115 method/Python_Rx/Data_rx.py
+++ Altera DE2-115 method/Python_Rx/Data_rx.py
@@ -1,7 +1,7 @@
 import serial
 
 # open the serial port at the appropriate baud rate and settings
-ser = serial.Serial('COM7', baudrate = 115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0.001)
+ser = serial.Serial('COM5', baudrate = 115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0.001)
 
 # Open file for writing
 with open('received_data.txt', 'w') as f:
@@ -24,23 +24,23 @@ with open('received_data.txt', 'w') as f:
             # Last 6 bits of the binary_data
             last_6bits = binary_data[2:]
 
-            # If first_2bits is not equal to prev_value_first_2bits, do the following
+            # If first_3bits is not equal to prev_value_first_3bits, do the following
             if first_2bits != prev_value_first_2bits:
-                # Circuit data is the last 6 bits of the binary_data concatanate for 2 steps starting from first_2bits = 00 to first_2bits = 01
+                # Circuit data is the last 5 bits of the binary_data concatanate for 2 steps starting from first_3bits = 000 to first_3bits = 001
                 if first_2bits == '00':
                     circuit_data = last_6bits
                 elif first_2bits == '01':
                     circuit_data = last_6bits + circuit_data
                 
 
-            # If circuit_data is not equal to prev_value (except for 000110111001 or 000000000000) and circuit_data is 10 bits long, print circuit_data and write to file
+            # If circuit_data is not equal to prev_value (except for 001101110010 or 000000000000) and circuit_data is 10 bits long, print circuit_data and write to file
             if len(circuit_data) == 12:
                 if circuit_data != prev_value:
                     print(circuit_data) # Print binary string 
                     f.write(circuit_data + "\n") # Write binary string to file
                     f.flush()  # flush write buffer to force data to be written to file
                     prev_value = circuit_data # Set prev_value to circuit_data
-                elif circuit_data == '000110111001' or circuit_data == '000000000000':
+                elif circuit_data == '001101110010' or circuit_data == '000000000000':
                     print(circuit_data) # Print binary string
                     f.write(circuit_data + "\n") # Write binary string to file
                     f.flush()  # flush write buffer to force data to be written to file
